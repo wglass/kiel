dab3b4e410bf6b834c2af5764483fb617bc15c4e
William Glass <william.glass@gmail.com>
2016-03-03 23:45:03 -0800

    Fix snappy tests to encode values properly.

--- tests/compression/snappy_tests.py
+++ tests/compression/snappy_tests.py
@@ -25,7 +25,7 @@ class SnappyCompressionTests(unittest.TestCase):
         )
 
     def test_compression_is_stable(self):
-        data = json.dumps({"foo": "bar", "blee": "bloo", "dog": "cat"})
+        data = json.dumps({"foo": "bar", "dog": "cat"}).encode("utf-8")
 
         data = snappy.compress(data)
         data = snappy.decompress(data)
@@ -33,14 +33,18 @@ class SnappyCompressionTests(unittest.TestCase):
         data = snappy.decompress(data)
 
         self.assertEqual(
-            json.loads(data),
-            {"foo": "bar", "blee": "bloo", "dog": "cat"}
+            json.loads(data.decode("utf-8")),
+            {"foo": "bar", "dog": "cat"}
         )
 
     def test_compression_includes_magic_header(self):
-        data = json.dumps({"foo": "bar", "blee": "bloo", "dog": "cat"})
+        data = json.dumps(
+            {"foo": "bar", "blee": "bloo", "dog": "cat"}).encode("utf-8")
         data = snappy.compress(data)
 
         header = struct.unpack_from("!bccccccbii", data)
 
-        self.assertEqual(header, (-126, 'S', 'N', 'A', 'P', 'P', 'Y', 0, 1, 1))
+        self.assertEqual(
+            header,
+            (-126, b'S', b'N', b'A', b'P', b'P', b'Y', 0, 1, 1)
+        )
